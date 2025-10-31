bdec55f01babdceccba72a3567b8888f01a1ce4b
Aishwarya-Kadam16 <aishwaryakadamm@gmail.com>
2025-10-31 23:49:58 +0530

    program1_1.c

--- Assignments/Assignment_1/program1_1.c
+++ Assignments/Assignment_1/program1_1.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
 
+/////////////////////////////////////////
+//
+//  Function Name : Divide
+//  Description :   It is used to print division of two numbers
+//  Input :         Int , Int
+//  Output :        Int
+//  Author :        Aishwarya Santosh Kadam
+//  Date :          31/10/2025
+//
+/////////////////////////////////////////
+
 int Divide(int iNo1 , int iNo2)
 {
     int iAns = 0;
@@ -11,6 +22,12 @@ int Divide(int iNo1 , int iNo2)
     iAns = iNo1 / iNo2;
     return iAns;
 }
+
+////////////////////////////////////////////////
+//
+//  Entry Point Function of the Application
+//
+//////////////////////////////////////////////// 
 
 int main()
 {
@@ -24,3 +41,4 @@ int main()
     return 0;
 
 }
+
