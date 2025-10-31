a13ec1aabcb4cba60e51a445d07310eacc817838
Aishwarya-Kadam16 <aishwaryakadamm@gmail.com>
2025-10-31 23:45:45 +0530

    program2_1.c

--- Assignments/Assignment_2/program2_1.c
+++ Assignments/Assignment_2/program2_1.c
@@ -5,7 +5,7 @@
 //  Function Name : Display
 //  Description :   It is used to print number of * on the screen
 //  Input :         Int
-//  Output :        Int
+//  Output :        void
 //  Author :        Aishwarya Santosh Kadam
 //  Date :          31/10/2025
 //
@@ -37,4 +37,5 @@ int main()
     Display(iValue);
 
     return 0;
+
 }
