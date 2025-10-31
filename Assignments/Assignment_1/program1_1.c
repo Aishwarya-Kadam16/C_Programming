7353bd04e4a03f4386125a8d73d4543af9702019
Aishwarya-Kadam16 <aishwaryakadamm@gmail.com>
2025-10-31 23:44:41 +0530

    program1_1.c

--- Assignments/Assignment_1/program1_1.c
+++ Assignments/Assignment_1/program1_1.c
@@ -15,11 +15,12 @@ int Divide(int iNo1 , int iNo2)
 int main()
 {
     int iValue1=15 , iValue2 = 5;
-    int iRet = 0;
+    int iRet =0;
 
     iRet = Divide(iValue1 , iValue2);
 
     printf("Division is %d", iRet);
 
     return 0;
+
 }
