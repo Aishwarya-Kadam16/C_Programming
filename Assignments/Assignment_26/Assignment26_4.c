0fc64f281b1197c1316ad13dec7ed4146600efa1
Aishwarya-Kadam16 <aishwaryakadamm@gmail.com>
2025-11-28 17:27:52 +0530

    Add files via upload
    Add Assignment_26 Solutions (Q1-Q5)

---
+++ Assignments/Assignment_26/Assignment26_4.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+    int iCnt =0;
+    
+
+    for(iCnt=1;iCnt<=iNo;iCnt++)
+    {
+        printf("#\t");
+        printf("%d\t",iCnt);
+        if(iCnt<=iNo)
+        {
+            printf("*\t");
+        }
+        
+        
+    }
+
+}
+int main()
+{
+    int iValue=0;
+    printf("Enter number of elements : ");
+    scanf("%d",&iValue);
+
+    Pattern(iValue);
+    return 0;
+}
